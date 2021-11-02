191e6eb4fc673a7387c5e7c3ec8a532d17ee4a37
Erik Brown <erik.brown@mybrightwheel.com>
2021-11-01 22:31:32 -0700

    Update to use SSL

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg')
 let config;
 if (process.env.DATABASE_URL) {
   config = {
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL + '?sslmode=require',
   };
 }
 
